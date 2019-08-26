7492051005acd63a7dcdac8429f87ffc7bb854c4
Valentina Hallefors <vhallefors@lco.global>
2019-08-26 11:21:41 -0700

    fix issue with displaying images

--- src/views/archive.vue
+++ src/views/archive.vue
@@ -5,13 +5,22 @@
 </template>
 
 <script>
+import { API, Auth } from 'aws-amplify'
+import ImageView from '@/components/ImageView'
 import ImageNavigationPanel from '@/components/ImageNavigationPanel'
+import { mapGetters } from 'vuex'
 
 export default {
   name: "archive",
   components: {
+      ImageView,
       ImageNavigationPanel
-  }
+  },
+  computed: {
+   ...mapGetters('auth', {
+        user: 'user'
+    }),
+  },
 }
 </script>
 
