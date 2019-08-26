fc11d086ca48b89b8384cdf622cd418ca4b2337d
jackie <jloerincs@lco.global>
2019-08-26 16:40:52 +0000

    Change layout of user images

--- src/components/ImageNavigationPanel.vue
+++ src/components/ImageNavigationPanel.vue
@@ -1,15 +1,39 @@
 <template>
     <div>      
         <p>Your images:</p>
-        <div v-for="(item, index) in user_images" :key="index">
-            {{index}}. {{item.url}}
-            <img 
-                style="width: 60px; height: 60px;"
-                v-bind:src="item.url"
-                v-bind:title="item.filename"
-            >
-        </div>
         <p>{{username}}</p>
+        <div>
+            <b-collapse class="card" aria-id="contentIdForA11y3">
+                <div 
+                    slot="trigger" 
+                    slot-scope="props"
+                    class="card-header"
+                    role="button"
+                    aria-controls="contentIdForA11y3">
+                    <p class="card-header-title">
+                        Your Images
+                    </p>
+                    <a class="card-header-icon">
+                        <b-icon
+                            :icon="props.open ? 'menu-down' : 'menu-up'">
+                        </b-icon>
+                    </a>
+                </div>
+                <div class="card-content" v-for="(item, index) in user_images" :key="index">
+                    <div class="content">
+                        <div>
+                            {{item.filename}}
+                        </div>
+                        <img 
+                            style="width: 100px; height: 100px;"
+                            v-bind:src="item.url"
+                            v-bind:title="item.filename"
+                        >
+                        <div class="is-divider" data-content="OR"></div>
+                    </div>
+                </div>
+            </b-collapse>
+        </div>
     </div>
 </template>
 
