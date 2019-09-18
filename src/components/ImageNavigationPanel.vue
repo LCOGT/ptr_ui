b6f2f7b978ff5954fc942cc9fb6211b62f54467a
jackie <jloerincs@lco.global>
2019-09-18 21:50:54 +0000

    Remove sites dropdown

--- src/components/ImageNavigationPanel.vue
+++ src/components/ImageNavigationPanel.vue
@@ -47,9 +47,11 @@
                                                 <p style="color:rgb(175,175,175);">
                                                     DEC: {{item.declination.toFixed(2)}}
                                                 </p>
-                                                <b-tooltip class="tooltip" label="Download FITS" :delay="1000" type="is-dark" position="is-left">
-                                                    <b-button class="button download-button" tag="a" :href="item.fits13_url" icon-pack="fas" icon-left="download"></b-button>
-                                                </b-tooltip>
+                                                <div v-if="item.ex13_fits_exists">
+                                                    <b-tooltip class="tooltip" label="Download EX13 FITS" :delay="1000" type="is-dark" position="is-left">
+                                                        <b-button class="button download-button" tag="a" :href="item.fits13_url" icon-pack="fas" icon-left="download"></b-button>
+                                                    </b-tooltip>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
@@ -186,7 +188,7 @@ export default {
 .side-panel {
   grid-column: 1;
   width: 520px;
-  height: 740px;
+  height: 650px;
 }
 .selected_thumbnail{
     background-color: rgb(60, 70, 70);
@@ -194,9 +196,6 @@ export default {
 .download-button{
     padding-bottom: 30px;
     width: 35px;
-}
-.tooltip{
-    padding-left: 0px;
 }
 </style>
 
