b25ec6cc59c5d40648d3ed508919726f0fc7c638
timbeccue <timothybeccue@gmail.com>
2019-10-11 12:17:24 -0700

    Refactor user-provided field values into one vuex module (command_params).

--- src/store/modules/command_params.js
+++ src/store/modules/command_params.js
@@ -2,11 +2,13 @@
  *  This module is responsible for tracking the state of fields holding parameters
  *  for commands (eg. exposure time).
  *  
- *  (oct 9) Some of these parameters are currently tracked in vuex module
- *  'observatory_configuration', and should be migrated to this module.
  */
 
 const state = {
+
+    // Mount parameters
+    mount_ra: '',
+    mount_dec: '',
 
     // Subframe parameters
     subframeIsActive: false,
@@ -16,10 +18,31 @@ const state = {
     subframe_x1: 1,
     subframe_y1: 1,
 
+    camera_areas_selection: '',
     camera_hint: '',
+    camera_exposure: '1',
+    camera_count: 1, // numberinput form requires number, not string. converted to string in expose command.
+    camera_area: null, 
+    camera_bin: '1', 
+    camera_dither: 'off',
+    camera_image_type: 'light',
+
+    filter_wheel_options_selection: '',
+
+    focuser_relative: '',
+    focuser_absolute: '',
+
+    rotator_relative: '',
+    rotator_absolute: '',
+
+    screen_brightness: 100,
 }
 
 const getters = {
+
+    mount_ra: state => state.mount_ra,
+    mount_dec: state => state.mount_dec,
+
     subframeIsActive: state => state.subframeIsActive,
     subframeDefinedWithFile: state => state.subframeDefinedWithFile,
     subframe_x0: state => state.subframe_x0,
@@ -27,25 +50,40 @@ const getters = {
     subframe_x1: state => state.subframe_x1,
     subframe_y1: state => state.subframe_y1,
 
+    camera_areas_selection: state => state.camera_areas_selection, 
     camera_hint: state => state.camera_hint,
+    camera_exposure: state => state.camera_exposure,
+    camera_count: state => state.camera_count,
+    camera_bin: state => state.camera_bin,
+    camera_dither: state => state.camera_dither,
+    camera_image_type: state => state.camera_image_type,
+
+    filter_wheel_options_selection: state => state.filter_wheel_options_selection,
+
+    focuser_relative: state => state.focuser_relative,
+    focuser_absolute: state => state.focuser_absolute,
+
+    rotator_relative: state => state.rotator_relative,
+    rotator_absolute: state => state.rotator_absolute,
+
+    screen_brightness: state => state.screen_brightness,
+
 }
 
 const actions = {
-    /**
-     *  Define a subframe with the top left and bottom right corners, and set
-     *  it to active (will apply to next image command).
-     */
-    defineSubframe({ commit }, {x0,y0,x1,y1}) {
-        commit('subframeIsActive', true)
-        commit('subframe_x0',x0)
-        commit('subframe_y0',y0)
-        commit('subframe_x1',x1)
-        commit('subframe_y1',y1)
+    // This action does nothing. It exists for easy reference.
+    empty({ commit }, payload) {
+        console.log('action called from command_params')
+        //console.log(payload)
+        //commit('mutation_name', value)
     },
-
 }
 
 const mutations = {
+
+    mount_ra(state, val) { state.mount_ra = val; },
+    mount_dec(state, val) { state.mount_dec = val; },
+
     subframeIsActive (state, val) { state.subframeIsActive = val },
     subframeDefinedWithFile (state, val) { state.subframeDefinedWithFile = val },
     subframe_x0 (state, val) { state.subframe_x0 = val; },
@@ -53,7 +91,23 @@ const mutations = {
     subframe_x1 (state, val) { state.subframe_x1 = val; },
     subframe_y1 (state, val) { state.subframe_y1 = val; },
 
+    camera_areas_selection (state, val) { state.camera_areas_selection = val; },
     camera_hint (state, val) { state.camera_hint = val; },
+    camera_exposure (state, val) { state.camera_exposure = val; },
+    camera_count (state, val) { state.camera_count = val; },
+    camera_bin (state, val) { state.camera_bin = val; },
+    camera_dither (state, val) { state.camera_dither = val; },
+    camera_image_type (state, val) { state.camera_image_type = val; },
+
+    filter_wheel_options_selection(state, val) { state.filter_wheel_options_selection = val; },
+
+    focuser_relative(state, val) { state.focuser_relative = val; },
+    focuser_absolute(state, val) { state.focuser_absolute = val; },
+
+    rotator_relative(state, val) { state.rotator_relative = val; },
+    rotator_absolute(state, val) { state.rotator_absolute = val; },
+
+    screen_brightness(state, val) { state.screen_brightness = val; },
 }
 
 export default {
