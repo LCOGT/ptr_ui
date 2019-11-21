c66723be961acd208637de403aa62e5759153a52
timbeccue <timothybeccue@gmail.com>
2019-11-21 11:36:36 -0800

    Remove old test code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,6 @@
     <!--the-world-map name="google" /-->
 
     <leaflet-map name="leafmap"></leaflet-map>
-    <div id="testplotdiv" style="width: 600px; height: 200px;" />
   </div>
 </template>
 
@@ -20,38 +19,6 @@ export default {
     TheWorldMap,
     LeafletMap
   },
-  mounted() {
-    let testplotdiv = document.getElementById("testplotdiv")
-    let xdata = [[1,3],[2,6],[3,8],[4,2]]
-    let opts = {
-      zoomStack: true,
-      selection: { mode: "xy" },
-      grid: {
-        color: "red",
-        //backgroundColor: "green",
-        borderColor: "orange",
-        margin: {
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-        },
-        axisMargin: 0,
-        labelMargin: 0,
-        minBorderMargin: 0,
-        borderWidth: 0,
-        hoverable: true,
-        markings: [ { xaxis: { from: 3, to: 3 }, yaxis: {from: 0, to: 8}, color: "white", lineWidth: 1}],
-      },
-      yaxis: {
-        show: false,
-      },
-      xaxis: {
-        show: false,
-      },
-    }
-    $.plot(testplotdiv, [xdata], opts);
-  }
 }
 </script>
 
