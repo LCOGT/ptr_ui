9cf915db80866f09a3c449eb87404b8f92628def
timbeccue <timothybeccue@gmail.com>
2019-12-16 15:22:30 -0800

    add empty calendar page

---
+++ src/views/calendar.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <div class="column is-full container">
+      <p>calendar</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'calendar',
+  components: {
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  margin-top: 3em;
+}
+</style>
