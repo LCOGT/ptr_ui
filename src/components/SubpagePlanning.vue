96925034b181fd9ac1a4191ebacc3af26a2400a1
timbeccue <timothybeccue@gmail.com>
2020-01-15 09:42:42 -0800

    Clean code and minor formatting.

--- src/components/SubpagePlanning.vue
+++ src/components/SubpagePlanning.vue
@@ -1,11 +1,7 @@
 <template><div>
 
-  <div class="demo-app container">
-    <div class="demo-app-top">
-    <!--week-of-events /-->
-    <the-calendar :calendarSite="sitecode" />
-    </div>
-    </div>
+    <week-of-events />
+    <!--the-calendar :calendarSite="sitecode" /-->
 
 </div></template>
 
@@ -28,18 +24,4 @@ export default {
 
 
 <style scoped>
-.demo-app {
-  font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
-  font-size: 14px;
-}
-
-.demo-app-top {
-  margin: 0 0 3em;
-}
-
-.demo-app-calendar {
-  margin: 0 auto;
-  max-width: 900px;
-}
-
 </style>
