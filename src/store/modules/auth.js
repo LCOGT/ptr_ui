156b1cc04210437d5bbe747076f0487d0f28f2a9
timbeccue <timothybeccue@gmail.com>
2020-01-31 10:51:20 -0800

    Send to new jobs api for testing

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -8,52 +8,52 @@ const state = {
 
 // getters
 const getters = {
-    user: state => state.user,
-    username: state => state.user.username,
-    isLoggedIn: state => state.user !== '',
-    getToken: state => {
-        try {
-            return state.user.signInUserSession.idToken.jwtToken
-        } catch {
-            return ''
-        }
-    }
+    //user: state => state.user,
+    //username: state => state.user.username,
+    //isLoggedIn: state => state.user !== '',
+    //getToken: state => {
+    //    try {
+    //        return state.user.signInUserSession.idToken.jwtToken
+    //    } catch {
+    //        return ''
+    //    }
+    //}
 }
 
 // actions
 const actions = {
-    log_in_user({ commit, dispatch }, user) {
-        commit('setUser', user)
-        dispatch('observatory_configuration/update_config', null, {root: true})
-        dispatch('images/refresh_latest_images', null, {root: true})
-    },
+    //log_in_user({ commit, dispatch }, user) {
+    //    commit('setUser', user)
+    //    dispatch('observatory_configuration/update_config', null, {root: true})
+    //    dispatch('images/refresh_latest_images', null, {root: true})
+    //},
 
-    logInUser({ commit, dispatch }, user_credentials) {
-        let username = user_credentials.username
-        let password = user_credentials.password
-        Auth.signIn({ username: username, password: password, region: 'us-west-2' })
-            .then(user => {
-                console.log(user)
-                commit('setUser', user)
-                dispatch('observatory_configuration/update_config', null, {root: true})
-                dispatch('images/refresh_latest_images', null, {root: true})
-            })
-            .catch(err => console.log(err))
-    },
+    //logInUser({ commit, dispatch }, user_credentials) {
+    //    let username = user_credentials.username
+    //    let password = user_credentials.password
+    //    Auth.signIn({ username: username, password: password, region: 'us-west-2' })
+    //        .then(user => {
+    //            console.log(user)
+    //            commit('setUser', user)
+    //            dispatch('observatory_configuration/update_config', null, {root: true})
+    //            dispatch('images/refresh_latest_images', null, {root: true})
+    //        })
+    //        .catch(err => console.log(err))
+    //},
 
-    logOutUser({ commit }) {
-        this.$auth.logout({
-            returnTo: window.location.origin
-        });
-    }
+    //logOutUser({ commit }) {
+    //    this.$auth.logout({
+    //        returnTo: window.location.origin
+    //    });
+    //}
 
 }
 
 // mutations
 const mutations = {
-    setUser(state, user) {
-        state.user = user
-    }
+    //setUser(state, user) {
+    //    state.user = user
+    //}
 }
 
 export default {
