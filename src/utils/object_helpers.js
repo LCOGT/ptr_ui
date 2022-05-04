756307bd407035c93f6735d77931b24774979f9e
Katie Ciurleo <katieciurleo@gmail.com>
2022-05-04 16:05:23 -0700

    Add common name parser

---
+++ src/utils/object_helpers.js
@@ -0,0 +1,18 @@
+/* Useful sky object related functions */
+var object_helpers = {
+    common_name_parse: function(object_name) {
+        /* Takes common names for objects and returns Aladin searchable object names
+        */
+
+        //List of potential alternate names in lowercase with corresponding proper Aladin names
+        const common_names = {"ngp": "North Galactic Pole", "sgp": "South Galactic Pole"}
+
+        if(common_names.hasOwnProperty(object_name.toLowerCase())) {
+            return common_names[object_name.toLowerCase()];
+        } else {
+            return object_name;
+        };
+    },
+}
+
+export default object_helpers
