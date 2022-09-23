06b7e488dc6280dee99786c165bf933f60d0b62d
Tim Beccue <timothybeccue@gmail.com>
2022-09-23 14:48:50 -0400

    Add specific linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,16 @@ module.exports = {
     es2021: true
   },
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/recommended', // for Vue 2.x
     'standard'
   ],
+  globals: {
+    "JS9": true,
+    "$": true,
+    "d3": true,
+    "google": true,
+    "OverlappingMarkerSpiderfier": true,
+  },
   overrides: [
   ],
   parserOptions: {
@@ -17,5 +24,14 @@ module.exports = {
     'vue'
   ],
   rules: {
+    "camelcase": "off",
+    "eqeqeq": "off",
+    "brace-style": "off",
+    "vue/require-prop-type-constructor": "off",
+    "vue/prop-name-casing": "off",
+    "vue/require-default-prop": "off",
+    "vue/require-prop-types": "off",
+    "vue/order-in-components": "off",
+    "vue/multi-word-component-names": "off",
   }
 }
