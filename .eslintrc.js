3218cbe8e0653f0ed4f9307bc3af512fc9c1f45a
Tim Beccue <timothybeccue@gmail.com>
2022-10-02 17:16:41 -0400

    Configure npm lint commands

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,12 +8,13 @@ module.exports = {
     'standard'
   ],
   globals: {
-    "A": true, // Aladin script
-    "JS9": true,
-    "$": true,
-    "d3": true,
-    "google": true,
-    "OverlappingMarkerSpiderfier": true,
+    A: true, // Aladin script
+    JS9: true,
+    $: true,
+    d3: true,
+    google: true,
+    OverlappingMarkerSpiderfier: true,
+    _chatlio: true
   },
   overrides: [
   ],
@@ -23,25 +24,25 @@ module.exports = {
   },
   plugins: [
     'vue',
-    'no-autofix',
+    'no-autofix'
   ],
   rules: {
-    "camelcase": "off",
-    "eqeqeq": "off",
-    "brace-style": "off",
-    "vue/require-prop-type-constructor": "off",
-    "vue/prop-name-casing": "off",
-    "vue/require-default-prop": "off",
-    "vue/require-prop-types": "off",
-    "vue/order-in-components": "off",
-    "vue/multi-word-component-names": "off",
+    'camelcase': 'off',
+    'eqeqeq': 'off',
+    'brace-style': 'off',
+    'vue/require-prop-type-constructor': 'off',
+    'vue/prop-name-casing': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/require-prop-types': 'off',
+    'vue/order-in-components': 'off',
+    'vue/multi-word-component-names': 'off',
 
     /* Don't automatically fix these when running `eslint --fix`, but keep the warning/error */
 
-    // This autofix induced breaking changes when props are named with a mix of camelCase and snake_case, 
-    // such as in SiteCalendar by changing the prop for TheCalendar (fc_timeZone) 
+    // This autofix induced breaking changes when props are named with a mix of camelCase and snake_case,
+    // such as in SiteCalendar by changing the prop for TheCalendar (fc_timeZone)
     // from 'fc_timeZone' to 'fc-time-zone'
-    "vue/attribute-hyphenation": "off",
-    "no-autofix/vue/attribute-hyphenation": "error",
+    'vue/attribute-hyphenation': 'off',
+    'no-autofix/vue/attribute-hyphenation': 'error'
   }
 }
