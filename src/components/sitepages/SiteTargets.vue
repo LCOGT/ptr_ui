c2616ebc1ddde5b90cf826ac2802f2dfcba53051
Lloyd Dakin <taidakin99lol@gmail.com>
2023-12-06 15:40:47 -0800

    auto sets fov for aladin finder using the camera_size_degrees getter we now automatically set the aladin finder window to be the same size as the camera's fov

--- src/components/sitepages/SiteTargets.vue
+++ src/components/sitepages/SiteTargets.vue
@@ -681,14 +681,13 @@ export default {
     this.$loadScript('https://aladin.u-strasbg.fr/AladinLite/api/v2/latest/aladin.min.js')
       .then(async () => {
         // Default: load aladin on m33, but use coords in mount fields if possible.
-        let target = 'M65'
+        let target = 'M17'
         if (parseFloat(this.mount_ra) && parseFloat(this.mount_dec)) {
           target = `${15 * this.mount_ra} ${this.mount_dec}`
         }
 
         // Defaults FOV of 1, use pixel size of camera if present
-        const fov = this.autoAladinFov()
-        console.log('fov: ' + fov)
+        const fov = this.camera_size_degrees
 
         // Initialize Aladin
         this.aladin = A.aladin('#aladin-lite-div', {
@@ -957,7 +956,6 @@ export default {
     },
 
     // calculates the fov based on the max side length for the sites camera, if parameters are undefined by site defaults to a fov of 1
-    // TODO Carolina is creating a get fov function that we will just import and reuse here or above, most likely a getter
     autoAladinFov () {
       const camConfig = this.$store.getters['site_config/get_camera_config']
       const pixelScale = camConfig?.settings?.onebyone_pix_scale
@@ -1062,7 +1060,8 @@ export default {
       'site_latitude',
       'site_longitude',
       'site_name',
-      'timezone'
+      'timezone',
+      'camera_size_degrees'
     ])
   }
 
